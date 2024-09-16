262de01cfcd93de8321e68f972d7b57a23f87424
sabunextdoor <sabuthwala@gmail.com>
2024-09-16 19:36:31 +0200

    Classes

---
+++ script.js
@@ -0,0 +1,13 @@
+class student{
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+}
+
+//objects
+//instance of a class
+var student1=new student("Sabu",15)
+
+    document.write(student1.name+"<br>")
+    document.write(student1.age)
